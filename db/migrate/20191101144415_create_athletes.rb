0fb0877456a133275e9a0b5e22f6a6006c864496
Erik Aylward <eaylward@mail.med.upenn.edu>
2019-11-01 13:54:06 -0400

    Get rid of username on Athletes table

--- db/migrate/20191101144415_create_athletes.rb
+++ db/migrate/20191101144415_create_athletes.rb
@@ -2,7 +2,6 @@ class CreateAthletes < ActiveRecord::Migration[6.0]
   def change
     create_table :athletes do |t|
       t.string :name
-      t.string :username
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 2019_11_01_144717) do
 
   create_table "athletes", force: :cascade do |t|
     t.string "name"
-    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
